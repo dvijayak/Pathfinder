020377a056d270a7d0539675457654bd74e893cb
Daniel VJ (Windows) <daniel.vkumar@gmail.com>
2019-11-08 18:39:23 -0500

    Initial commit. Working away at a basic pathfinder demo.

---
+++ Assets/Scripts/Pathfinder.cs
@@ -0,0 +1,115 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pathfinder : MonoBehaviour
+{
+    public float cellSize = 1f;
+
+    TileGraph graph;
+
+    GameObject ground;
+
+    Tile[,] grid;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ground = GameObject.Find("Ground");
+        if (ground == null)
+        {
+            throw new UnityException("No Ground found");
+        }
+
+        MakeGraph();
+
+        // TEST: Force the test path to be drawn
+        graph.Start = Vector2.up;
+        graph.End = Vector2.one;
+    }
+
+    //
+    // Summary:
+    // Divide the x-z plane of the ground into a grid given the cell size
+    void MakeGraph()
+    {
+        // TODO
+        Bounds bounds = ground.GetComponent<Renderer>().bounds;
+
+        Vector3 xzBoundsCenter = bounds.center + new Vector3(0, bounds.extents.y, 0);
+        Vector3 gridTopLeft = xzBoundsCenter + new Vector3(-bounds.extents.x, 0, bounds.extents.z);        
+
+        Vector2 gridArea = new Vector2(bounds.size.x, bounds.size.z);
+        int maxRowCount = (int)Mathf.Floor(gridArea.x / cellSize);
+        int maxColCount = (int)Mathf.Floor(gridArea.y / cellSize);
+
+        Tile[,] grid = new Tile[maxRowCount, maxColCount];
+
+        for (int r = 0; r < grid.GetLength(0); r++)
+        {
+            for (int c = 0; c < grid.GetLength(1); c++)
+            {
+                // World space point that corresponds to the center of the tile
+                Vector3 worldPoint = gridTopLeft + new Vector3(c + cellSize/2, 0, -(r + cellSize/2));
+
+                // Determine overlapping obstacles
+                TileType tileType = TileType.Free;
+                Collider[] colliders = Physics.OverlapSphere(worldPoint, cellSize);
+                if (colliders != null && colliders.Length > 0)
+                {
+                    bool foundOurself = false;
+                    for (int i = 0; i < colliders.Length; i++)
+                    {
+                        if (colliders[i] == ground.GetComponent<Collider>())
+                        {
+                            foundOurself = true;
+                            break;
+                        }
+                    }
+                    if (!foundOurself)
+                    {
+                        tileType = TileType.Obstacle;
+                    }                    
+                }
+
+                grid[r, c] = new Tile(new Vector2(r, c), tileType, worldPoint);
+            }
+        }
+
+        graph = new TileGraph(grid);
+
+        // TODO: Remove after debug
+        this.grid = grid;
+    }
+
+    // TODO: Debug only
+    private void OnDrawGizmos()
+    {
+        if (this.grid != null)
+        {            
+            foreach (Tile tile in grid)
+            {
+                Gizmos.color = tile.Type == TileType.Obstacle ? Color.red : Color.yellow;
+                Gizmos.DrawSphere(tile.WorldRegion, cellSize);
+            }            
+        }        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (graph.Start.HasValue && graph.End.HasValue)
+        {            
+            List<Tile> path = graph.ComputePath(graph.Start.GetValueOrDefault(), graph.End.GetValueOrDefault());
+            
+            // TODO: Draw better path, not just a stupid debug line            
+            for (int i = 1; i < path.Count; i++)
+            {
+                Debug.DrawLine(path[i - 1].WorldRegion, path[i].WorldRegion, Color.blue, 300f);
+            }
+
+            graph.Start = null;
+            graph.End = null;
+        }
+    }
+}
---
+++ Assets/Scripts/TileGraph.cs
@@ -0,0 +1,181 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public enum TileType
+{
+    Free = 0,
+    Obstacle,
+    Start,
+    End
+}
+
+public interface IMovementCostComputer
+{
+    // Cost of moving from tile of type `from` to a neighboring tile of type `to`
+    float CostOfMovement(TileType from, TileType to);
+}
+
+public class UnitarianMovementCost : IMovementCostComputer
+{
+    static UnitarianMovementCost _Instance;
+    public static UnitarianMovementCost Instance
+    {
+        get
+        {
+            if (_Instance == null)
+            {
+                _Instance = new UnitarianMovementCost();
+            }
+            return _Instance;
+        }
+    }
+
+    private UnitarianMovementCost() {}
+
+    public float CostOfMovement(TileType from, TileType to)
+    {
+        return 1f;
+    }
+}
+
+public class Tile
+{
+    public Vector2 Index { get; }
+    public TileType Type { get; }
+    // TODO: World region thingy...for now, just store the point
+    public Vector3 WorldRegion { get; }
+
+    public Tile(Vector2 index, TileType type, Vector3 region)
+    {
+        Index = index;
+        Type = type;
+        WorldRegion = region;
+    }
+}
+
+public class TileGraph
+{
+    Tile startTile;
+    Vector2? _Start;
+    public Vector2? Start
+    {
+        get
+        {
+            return _Start;
+        }
+
+        set
+        {
+            _Start = value;
+            if (_Start.HasValue)
+            {
+                startTile = grid[(int)_Start.GetValueOrDefault().x, (int)_Start.GetValueOrDefault().y];
+            }
+            else
+            {
+                startTile = null;
+            }
+        }
+    }
+
+    Tile endTile;
+    Vector2? _End;
+    public Vector2? End
+    {
+        get
+        {
+            return _End;
+        }
+
+        set
+        {
+            _End = value;
+            if (_End.HasValue)
+            {
+                endTile = grid[(int)_End.GetValueOrDefault().x, (int)_End.GetValueOrDefault().y];
+            }            
+            else
+            {
+                endTile = null;
+            }
+        }
+    }
+
+    Tile[,] grid;
+
+    public int RowCount { get { return grid.GetLength(0); } }
+    public int ColCount { get { return grid.GetLength(1); } }
+    public int TileCount { get { return grid.Length; } }
+
+    IMovementCostComputer movementCostComputer;
+
+    public TileGraph(Tile[,] grid)
+    {
+        this.grid = grid;
+        this.movementCostComputer = UnitarianMovementCost.Instance;
+    }
+
+    public TileGraph(Tile[,] grid, IMovementCostComputer movementCostComputer)
+    {
+        this.grid = grid;
+        this.movementCostComputer = movementCostComputer;
+    }
+
+    public Tile Tile(Vector2 index)
+    {
+        return grid[(int)index.x, (int)index.y];
+    }
+
+    public Tile Tile(int row, int col)
+    {
+        return grid[row, col];
+    }
+
+    List<Tile> Neighbors(Vector2 index)
+    {
+        // TODO
+        return new List<Tile>();
+    }
+
+    List<Tile> UnobstructedNeighbors(Vector2 index)
+    {
+        List<Tile> neighbors = Neighbors(index);
+        neighbors.RemoveAll(tile => tile.Type == TileType.Obstacle);
+        return neighbors;
+    }
+
+    float CostOfMovement(Vector2 from, Vector2 to)
+    {
+        return movementCostComputer.CostOfMovement(Tile(from).Type, Tile(to).Type);
+    }
+
+    float HeuristicToEnd(Vector2 from)
+    {
+        if (!End.HasValue)
+        {
+            throw new UnityException("End tile is expected to be set");
+        }
+
+        // Manhattan distance
+        return Mathf.Abs(End.GetValueOrDefault().x - from.x) + Mathf.Abs(End.GetValueOrDefault().y - from.y);
+    }
+
+    public List<Tile> ComputePath(Vector2 from, Vector2 to)
+    {
+        List<Tile> path = new List<Tile>();
+
+        // TODO
+
+        //// TEST
+        //int cellCount = Random.Range(0, TileCount);
+        //for (int i = 0; i < cellCount; i++)
+        //{
+        //    int r = (int)Random.Range(0, RowCount);
+        //    int c = (int)Random.Range(0, ColCount);
+        //    path.Add(Tile(r, c));
+        //}
+
+        return path;
+    }
+}
